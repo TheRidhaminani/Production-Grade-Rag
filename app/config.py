08d93e871f6f3348767fea233b7c792be795939b
Ridham Inani <inaniridham@gmail.com>
2026-08-23 12:20:24 +0530

    Initial commit: Production Grade RAG system

---
+++ app/config.py
@@ -0,0 +1,43 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+class Settings:
+    # --- GEMINI EMBEDDINGS ---
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+    # --- VECTOR DB (QDRANT) ---
+    QDRANT_URL = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_COLLECTION = "enterprise_rag"
+
+    # --- REASONING ENGINE (GROQ) ---
+    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_MODEL = "openai/gpt-oss-20b"
+    GROQ_FALLBACK_API_KEY = os.getenv("GROQ_FALLBACK_API_KEY")
+
+    # --- LLM GATEWAY (PORTKEY) ---
+    PORTKEY_API_KEY = os.getenv("PORTKEY_API_KEY")
+    GROQ_SLUG =  "productionrag"     # primary: @productionrag/openai/gpt-oss-20b
+    GROQ_SLUG_2 = "productionrag"  # fallback: @productionrag/openai/gpt-oss-20b
+    PORTKEY_CONFIG_SLUG = os.getenv("PORTKEY_CONFIG_SLUG")
+
+    
+    # --- OBSERVABILITY ---
+    LANGSMITH_TRACING = os.getenv("LANGSMITH_TRACING", "true")
+    LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
+    LANGSMITH_PROJECT = os.getenv("LANGSMITH_PROJECT", "rag_scale_test")
+    LANGSMITH_ENDPOINT = os.getenv("LANGSMITH_ENDPOINT", "https://api.smith.langchain.com")
+
+# Apply LangChain environment variables for automatic tracing
+os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGSMITH_TRACING", "true")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGSMITH_API_KEY", "")
+os.environ["LANGCHAIN_PROJECT"] = os.getenv("LANGSMITH_PROJECT", "rag_scale_test")
+os.environ["LANGCHAIN_ENDPOINT"] = os.getenv("LANGSMITH_ENDPOINT", "https://api.smith.langchain.com")
+
+settings = Settings()
+# whenever this particular config is loaded we are going to load this
+# with the help of this particular variable
+# is class ko call kar leta h to iske andar k sare variables aa jate h
